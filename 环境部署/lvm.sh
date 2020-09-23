9f257e8d7e9d39049ed8d4ab7c553686f29abcaa
JASON-yul <63461007+JASON-yul@users.noreply.github.com>
2020-09-23 15:59:24 +0800

    Update and rename lvmcreate.sh to lvm.sh

--- 环境部署/lvm.sh
+++ 环境部署/lvm.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
 DISK1="/dev/vdb"
-DISK2="/dev/vdc"
 VGNAME="data_disk"
 LVNAME="data_disk"
 #创建pv
 pvcreate $DISK1
-pvcreate $DISK2
+
 
 #创建vg
-vgcreate $VGNAME $DISK1 $DISK2
+vgcreate $VGNAME $DISK1 
 
 #创建lv
 lvcreate -n $LVNAME -l +100%FREE $VGNAME
