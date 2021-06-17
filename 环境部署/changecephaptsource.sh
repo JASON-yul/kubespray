71fda581f638d724125cb406470abeaafbf2790d
JASON-yul <63461007+JASON-yul@users.noreply.github.com>
2021-06-17 16:38:19 +0800

    Update changecephaptsource.sh

--- 环境部署/changecephaptsource.sh
+++ 环境部署/changecephaptsource.sh
@@ -16,3 +16,22 @@ deb-src http://mirrors.aliyun.com/ubuntu/ eoan-proposed main restricted universe
 deb http://mirrors.aliyun.com/ubuntu/ eoan-backports main restricted universe multiverse
 deb-src http://mirrors.aliyun.com/ubuntu/ eoan-backports main restricted universe multiverse
 EOF
+
+
+注：Ubuntu18.04系统
+
+deb http://mirrors.aliyun.com/ubuntu/ bionic main restricted universe multiverse
+deb-src http://mirrors.aliyun.com/ubuntu/ bionic main restricted universe multiverse
+
+deb http://mirrors.aliyun.com/ubuntu/ bionic-security main restricted universe multiverse
+deb-src http://mirrors.aliyun.com/ubuntu/ bionic-security main restricted universe multiverse
+
+deb http://mirrors.aliyun.com/ubuntu/ bionic-updates main restricted universe multiverse
+deb-src http://mirrors.aliyun.com/ubuntu/ bionic-updates main restricted universe multiverse
+
+deb http://mirrors.aliyun.com/ubuntu/ bionic-proposed main restricted universe multiverse
+deb-src http://mirrors.aliyun.com/ubuntu/ bionic-proposed main restricted universe multiverse
+
+deb http://mirrors.aliyun.com/ubuntu/ bionic-backports main restricted universe multiverse
+deb-src http://mirrors.aliyun.com/ubuntu/ bionic-backports main restricted universe multiverse
+deb http://mirrors.aliyun.com/ceph/debian-nautilus/ bionic main
